7ba3d57fb651cd593d4636cbfe92b9f13826ca98
Patryk Łowigus <101129079+plowigus@users.noreply.github.com>
2026-02-02 19:37:56 +0100

    pusher fix

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -8,16 +8,13 @@ export const pusherServer = new PusherServer({
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
     useTLS: true,
 });
-
-/**
- * Singleton pattern for PusherClient to prevent multiple connections
- * during hot-reloading in development.
- */
 const globalForPusher = globalThis as unknown as { pusherClient: PusherClient | undefined };
 
-export const pusherClient = globalForPusher.pusherClient ?? new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-    authEndpoint: "/api/pusher/auth",
-});
+export const pusherClient = (typeof window !== "undefined"
+    ? (globalForPusher.pusherClient ?? new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+        authEndpoint: "/api/pusher/auth",
+    }))
+    : {}) as PusherClient;
 
-if (process.env.NODE_ENV !== "production") globalForPusher.pusherClient = pusherClient;
+if (typeof window !== "undefined" && process.env.NODE_ENV !== "production") globalForPusher.pusherClient = pusherClient;
